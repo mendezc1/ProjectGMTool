035aa76c5084e86560ed0fb89ff3e9cf74dcf78d
mendezc1 <mendezc@oregonstate.edu>
2016-06-14 10:30:22 -0700

    Screenshot preview working as proof of concept

--- background.js
+++ background.js
@@ -14,7 +14,7 @@ var screenShotURL;
 		function(request, sender, sendResponse) {
 			if (request.greeting == "takeScreenShot"){	
 			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  				chrome.tabs.sendMessage(tabs[0].id, {callFunction: "renderImage"}, function(response) { });
+  			//	chrome.tabs.sendMessage(tabs[0].id, {callFunction: "renderImage"}, function(response) { });
 			});
 				takeScreenShot();
 				chrome.extension.getBackgroundPage().console.log("After image");		
--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -4,6 +4,13 @@ function takeScreenShot() {
 	chrome.windows.getCurrent(function (win) {    
     	chrome.tabs.captureVisibleTab(win.id,{"format": "png"}, function(imgUrl) {	
     		//var myImg = document.getElementById("previewImage");
+			//var myCanvas = document.create("canvas");
+        	//var myImg = document.create("img");
+        	//console.log("image url", myImg);
+        	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  				chrome.tabs.sendMessage(tabs[0].id, {callFunction: "renderImage", imageUrl: imgUrl}, function(response) { });
+			});
+        	//window.open(imgUrl);
         	//myImg.src = imgUrl;
         });    
 	});
